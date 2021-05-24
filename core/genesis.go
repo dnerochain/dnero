2c9de5d3048f747f371d7ccbb524f7d46daa15b7
Muroko <muroko@rodzisso.com>
2021-05-24 02:13:02 -0400

    testchain chainID set
    Setting TestChain ChainID

--- core/genesis.go
+++ core/genesis.go
@@ -1,8 +1,19 @@
 package core
 
+/** MAINNET - GENESIS **/
 const (
 	MainnetChainID = "mainnet"
 
+	MainnetGenesisBlockHash = "0x00"
+
+	GenesisBlockHeight = uint64(0)
+)
+
+/** TESTCHAIN - GENESIS v0.X.X **/
+const (
+	MainnetChainID = "testchain"
+
+    //TestChain Genesis
 	MainnetGenesisBlockHash = "0x00"
 
 	GenesisBlockHeight = uint64(0)
