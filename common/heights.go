ace0d103eb3b88e320a8f31ccddb296ced92de2e
Muroko <muroko@rodzisso.com>
2021-06-12 13:43:41 -0400

    New Block Heights Fork Update
    -Forks set with 100 blocks between

--- common/heights.go
+++ common/heights.go
@@ -1,19 +1,19 @@
 package common
 
-// HeightEnableValidatorReward specifies the minimal block height to enable the validtor DFUEL reward
+// HeightEnableValidatorReward specifies the minimal block height to enable the Validator DFUEL reward
 const HeightEnableValidatorReward uint64 = 101 // block #101
 
 // HeightEnableDneroV1 specifies the minimal block height to enable the DneroV1 feature.
-const HeightEnableDneroV1 uint64 = 102 // block #102
+const HeightEnableDneroV1 uint64 = 201 // block #201
 
 // HeightLowerGNStakeThresholdTo100 specifies the minimal block height to lower the GN Stake Threshold to 100 DNERO
-//const HeightLowerGNStakeThresholdTo100 uint64 = 103 // block #103 //StakeDeposit Fork Removed
+//const HeightLowerGNStakeThresholdTo100 uint64 = ### // block #000 //GN StakeDeposit Fork Removed
 
-// HeightEnableSmartContract specifies the minimal block height to eanble the Turing-complete smart contract support
-const HeightEnableSmartContract uint64 = 104 // block #104
+// HeightEnableSmartContract specifies the minimal block height to enable the Turing-complete smart contract support
+const HeightEnableSmartContract uint64 = 401 // block #401
 
-// HeightSampleStakingReward specifies the block heigth to enable sampling of staking reward
-const HeightSampleStakingReward uint64 = 105 // block #105
+// HeightSampleStakingReward specifies the block height to enable sampling of staking reward
+const HeightSampleStakingReward uint64 = 501 // block #501
 
 // CheckpointInterval defines the interval between checkpoints.
 const CheckpointInterval = int64(100)
