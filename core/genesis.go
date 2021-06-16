d396df9938d37e6f0f524bb063b168b129204362
Muroko <muroko@rodzisso.com>
2021-06-16 12:42:31 -0400

    NEW: Testchainnet Genesis Hash
    Testchainnet official Genesis Launch

--- core/genesis.go
+++ core/genesis.go
@@ -13,7 +13,8 @@ package core
 const (
 	MainnetChainID = "testchainnet"
 
-	MainnetGenesisBlockHash = "0x735f6287ec715d36e024f40993434386e0b715cdc99f9fcf3a696e3ba6678e93"
+	//MainnetGenesisBlockHash = "0x735f6287ec715d36e024f40993434386e0b715cdc99f9fcf3a696e3ba6678e93"
+	MainnetGenesisBlockHash = "0xd5af709911cbcdf5eef4c731a10586dc8953b96a0d81389b4dbe4c79e93e0864"
 
 	GenesisBlockHeight = uint64(0)
 )
