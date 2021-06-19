22d59a6be2eac4968d55d2a11ecd5f72617a54e7
Muroko <muroko@rodzisso.com>
2021-06-19 09:59:10 -0400

    SET: Current Testchainnet Genesis Hash

--- core/genesis.go
+++ core/genesis.go
@@ -13,8 +13,9 @@ package core
 const (
 	MainnetChainID = "testchainnet"
 
-	//MainnetGenesisBlockHash = "0x735f6287ec715d36e024f40993434386e0b715cdc99f9fcf3a696e3ba6678e93"
-	MainnetGenesisBlockHash = "0xd5af709911cbcdf5eef4c731a10586dc8953b96a0d81389b4dbe4c79e93e0864"
+	//MainnetGenesisBlockHash = "0x735f6287ec715d36e024f40993434386e0b715cdc99f9fcf3a696e3ba6678e93" //Temp
+	//MainnetGenesisBlockHash = "0xd5af709911cbcdf5eef4c731a10586dc8953b96a0d81389b4dbe4c79e93e0864" //Test
+	MainnetGenesisBlockHash = "0x7d59faedaab82e8d25bc570078ec90a4f65e852fa2e1e617ef089d39ac3e9101" //Current
 
 	GenesisBlockHeight = uint64(0)
 )
