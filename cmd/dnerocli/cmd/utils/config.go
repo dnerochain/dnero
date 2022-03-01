9e5dbf4b3f6b3d899d4ca4c2b38c60427111536f
Muroko <murokomail@gmail.com>
2022-03-01 06:37:27 -0500

    SET: Dnerocli "cmd" Directory

---
+++ cmd/dnerocli/cmd/utils/config.go
@@ -0,0 +1,14 @@
+package utils
+
+import "github.com/spf13/viper"
+
+const (
+	CfgRemoteRPCEndpoint = "remoteRPCEndpoint"
+	CfgDebug             = "debug"
+)
+
+func init() {
+	//viper.SetDefault(CfgRemoteRPCEndpoint, "http://localhost:16888/rpc") //DEL
+	viper.SetDefault(CfgRemoteRPCEndpoint, "http://localhost:15511/rpc")
+	viper.SetDefault(CfgDebug, false)
+}
