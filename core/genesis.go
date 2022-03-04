0cef1e269edc1561533debb5c98350c640c8a982
Muroko <murokomail@gmail.com>
2022-03-04 10:12:32 -0500

    ADD: Added Testchainnet Genesis Block Hash

--- core/genesis.go
+++ core/genesis.go
@@ -1,9 +1,10 @@
 package core
 
 const (
-	MainnetChainID = "mainnet"
+	//MainnetChainID = "mainnet"
+	MainnetChainID = "testchainnet"
 
-	MainnetGenesisBlockHash = "0x"
+	MainnetGenesisBlockHash = "0x26622ec892be4c2f04d24405cd4c3cb92454050afb4fc01d75759f53f77edf03"
 
 	GenesisBlockHeight = uint64(0)
 )
