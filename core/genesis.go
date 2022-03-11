9e7a91d8163aa5c4b83221366cf14b0496b6d692
Muroko <murokomail@gmail.com>
2022-03-11 16:05:00 -0500

    v0.8.0

--- core/genesis.go
+++ core/genesis.go
@@ -1,10 +1,20 @@
 package core
 
+/* MAINNET */
+//const (
+//	MainnetChainID = "mainnet"
+//
+//	MainnetGenesisBlockHash = "0x000"
+//
+//	GenesisBlockHeight = uint64(0)
+//)
+
+/* TESTCHAINNET */
 const (
-	//MainnetChainID = "mainnet"
 	MainnetChainID = "testchainnet"
 
 	MainnetGenesisBlockHash = "0xb37adf0fb8480651fc53699a1b52e08aa79880a24abee4c0b329a90274c9ba71"
 
 	GenesisBlockHeight = uint64(0)
 )
+
