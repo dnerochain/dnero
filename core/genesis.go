e4d9959e6d5a79840ac0a1c0e4429b93e41693af
Muroko <murokomail@gmail.com>
2023-06-05 11:52:10 -0400

    v1.1.1
    CHANGES:
    New Genesis and Snapshot created.
    Updated Snapshot File

--- core/genesis.go
+++ core/genesis.go
@@ -4,7 +4,7 @@ package core
 const (
 	MainnetChainID = "mainnet"
 
-	MainnetGenesisBlockHash = "0x2693b94e3a2ae55626c0d9cbcf1af2f631ba3b4af7a21a03ea9750bacf974041"
+	MainnetGenesisBlockHash = "0x63092e20f5891732ca0da3fe4902de22911ea0deafd09450bea8f597c3eaa5e9"
 
 	GenesisBlockHeight = uint64(0)
 )
